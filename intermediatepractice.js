854950c67094b5270d28e45be880188ae6809e3c
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-09 14:43:18 -0500

    Create intermediatepractice.js

---
+++ intermediatepractice.js
@@ -0,0 +1,110 @@
+# KWK-Javascript-Practice
+javascript practice from KWK: functions, objects, arrays, dom
+// TRY IT #1
+
+// Guess What will evaluate when each line runs! 
+console.log(5===5);
+ console.log(5>4);
+ console.log(5>=4);
+ console.log(5<4);
+console.log(5==="5");
+ console.log(5!=="5");
+ console.log(5!==4);
+ console.log(6!==5)
+ console.log(6!=="5")
+ console.log(6!=="6")
+
+
+
+
+
+//  Declare two variables - firstName and age, and assign them to appropriate values. 
+var firstName="Ankita";
+var age = 13;
+
+var lastName="Saha"
+var ages = 14
+
+console.log(ages===15)
+console.log(ages!==7)
+console.log(ages>=12)
+
+// Now, check for the following things:
+// Is your name the same as "Karlie"?
+console.log(firstName==="Karlie")
+// Is your age the same as 15?
+console.log(age!==15);
+// Is your age not the same as 15?
+console.log(13===13)
+// Is your age greater than 7?
+console.log(age!==7)
+// Is your age less than or equal to 10?
+// Is your age less than or equal to 10?
+// What is the remainder when you age is divided by 2?
+console.log(age%6)
+
+
+
+
+// TRY IT #2
+// Conditionals
+var school = "I'm stressed" 
+if(school === "I'm doing well")
+{console.log("Good for you!");}
+else if (school === "I'm stressed")
+{console.log("Calm down");}
+else {
+  console.log("you are ok");
+  }
+
+
+
+
+
+
+
+
+var sleep = "I am tired"
+if (sleep==="I am tired") 
+{
+console.log("go to sleep!");
+}
+else if(sleep==="I am not tired i guess")
+{
+  console.log("fine watch one more episode");
+  }
+  else {console.log("just go to sleep");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var sleepy = "I'm tired!"
+if (sleepy==="I'm tired!") 
+{console.log("go to bed.");}
+ else {console.log("One more episode.");}
+
+var weather = "Sunny";
+
+if (weather==="Rainy"){
+  console.log("Get a jacket!");
+} else if (weather==="Tornado"){console.log("Batton down the hatches. Run for your life!");
+} else {
+  console.log("The weather is fine.");
+}
