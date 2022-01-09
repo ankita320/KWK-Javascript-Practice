9cf120b1607e098c991afedd7ea96529c7328c0a
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-09 16:20:27 -0500

    Create introtoobjects.js

---
+++ introtoobjects.js
@@ -0,0 +1,37 @@
+# KWK-Javascript-Practice
+javascript practice from KWK: functions, objects, arrays, dom
+var friends = ["Arpita", "Mom", "Dad"];
+var bdays = ["August 20", "July 5", "April 10"];
+console.log(friends[1], bdays[1]);
+
+
+// Friends and their Birthdays... ONE ARRAYS
+
+var friendBday = [  [],[],[]  ];
+
+
+// Friends and their Birthdays...  ONE OBJECT
+
+var friendBdayObject = {
+  ankita: "June 16",
+  arpita: "August 20"
+  };
+console.log(friendBdayObject);
+console.log(friendBdayObject.ankita);
+console.log(friendBdayObject.arpita);
+
+var candiesInJar = {
+  snickers: "56",
+  hersheys: "45"
+};
+
+console.log(candiesInJar.snickers);
+
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️ ACESSING ELEMENTS IN AN OBJECT
+
+// object.key ==> value
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
